c9211754270c120d427b1d2703fc378de79ab590
ABHIJIT <ABHIJIT@STARK>
2022-11-21 10:44:54 +0530

    Abhijit Adsul - First Commit

---
+++ org.GitHub/src/test/java/Package/TestClass1.java
@@ -0,0 +1,34 @@
+package Package;
+
+import org.testng.annotations.Test;
+
+public class TestClass1 {
+
+	public class CoreAnnotations {
+
+		@Test
+		public void TC01() {
+			System.out.println("TC01");
+		}
+		
+		@Test
+		public void TC02() {
+			System.out.println("TC02");
+		}
+		
+		@Test
+		public void TC03() {
+			System.out.println("TC03s");
+		}
+		@Test
+		public void TC04() {
+			System.out.println("TC04");
+		}
+		@Test
+		public void TC05() {
+			System.out.println("TC05");
+		}
+		
+	}
+
+}
